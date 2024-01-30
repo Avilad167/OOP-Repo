c0eefac152c0cdf20b95c29a9cdaba2ab7023ab2
avi lad <A.Lad2535@student.leedsbeckett.ac.uk>
2024-01-30 11:18:34 +0000

    Inital commit

--- WeeklyExercises/1GettingStarted/PersonalDetails.java
+++ WeeklyExercises/1GettingStarted/PersonalDetails.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class PersonalDetails {
+class PersonalDetails {
+    public static void main(String[] args) {
+
+        String name = "Srijan";
+        String address = "123 Main Street, Cityville";
+        int age = 25;
+        String phoneNumber = "77784758631";
+        System.out.println("Name: " + name);
+        System.out.println("Address: " + address);
+        System.out.println("Age: " + age);
+        System.out.println("Phone Number: " + phoneNumber);
+    }
 }
